46a966022bb60495b7381228396d207fd5e24929
Alexander Brown <alex.brown@brownhaus.xyz>
2021-07-27 18:24:18 -0600

    Added my username to lock out

--- config.def.h
+++ config.def.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "alex";
+static const char *group = "alex";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
